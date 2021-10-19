e6a5715ac41d13a667c30d0b4b636dc8849df4c5
Brandon Fillpot <brandonfillpot@gmail.com>
2021-10-18 21:43:01 -0700

    books table created

--- db/init_db.js
+++ db/init_db.js
@@ -4,15 +4,51 @@ const {
   // other db methods 
 } = require('./index');
 
-async function buildTables() {
+const dropTables = async () => {
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
+      DROP TABLE IF EXISTS books;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
+}
+
+const createTables = async () => {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE books (
+        id SERIAL PRIMARY KEY,
+        title varchar(255) UNIQUE NOT NULL,
+        author varchar(255) NOT NULL,
+        genre varchar(255) NOT NULL,
+        description varchar(255) NOT NULL
+      );
+      `)
+
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
+}
+
+const rebuildDB = async () => {
   try {
     client.connect();
 
-    // drop tables in correct order
-
-    // build tables in correct order
-
+    await dropTables();
+    await createTables();
   } catch (error) {
+    console.log("Error during rebuildDB")
     throw error;
   }
 }
@@ -25,7 +61,7 @@ async function populateInitialData() {
   }
 }
 
-buildTables()
+rebuildDB()
   .then(populateInitialData)
   .catch(console.error)
   .finally(() => client.end());
