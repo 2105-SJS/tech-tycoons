9110dc2c93ff153e56c3f37de9ad0c538b1a0b63
Brandon Fillpot <brandonfillpot@gmail.com>
2021-10-19 12:22:29 -0700

    users table created

--- db/init_db.js
+++ db/init_db.js
@@ -9,7 +9,8 @@ const dropTables = async () => {
     console.log("Starting to drop tables...");
 
     await client.query(`
-      DROP TABLE IF EXISTS books;
+      DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS products;
     `);
 
     console.log("Finished dropping tables!");
@@ -24,14 +25,28 @@ const createTables = async () => {
     console.log("Starting to build tables...");
 
     await client.query(`
-      CREATE TABLE books (
+      CREATE TABLE products (
         id SERIAL PRIMARY KEY,
         title varchar(255) UNIQUE NOT NULL,
         author varchar(255) NOT NULL,
         genre varchar(255) NOT NULL,
-        description varchar(255) NOT NULL
+        description varchar(255) NOT NULL,
+        price varchar(255) NOT NULL,
+        "inStock" BOOLEAN NOT NULL DEFAULT false,
+        "imgURL" varchar(255) DEFAULT 'https://www.eduprizeschools.net/wp-content/uploads/2016/06/No_Image_Available.jpg'
+        );
+
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        "firstName" varchar(255) NOT NULL,
+        "lastName" varchar(255) NOT NULL,
+        email varchar(255) UNIQUE NOT NULL,
+        "imgURL" varchar(255) DEFAULT 'https://www.eduprizeschools.net/wp-content/uploads/2016/06/No_Image_Available.jpg',
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) UNIQUE NOT NULL,
+        "isAdmin" BOOLEAN NOT NULL DEFAULT false
       );
-      `)
+      `);
 
 
     console.log("Finished building tables!");
