47e05877e12a5ca0e6cae530127b214be56136f1
Kevin Kepner <kevintkepner@gmail.com>
2021-10-25 18:25:06 -0700

    productsapi

--- src/api/index.js
+++ src/api/index.js
@@ -20,3 +20,37 @@ export async function getAllProducts() {
   }
 }
 
+export async function getAllProducts() {
+  try {
+    const { data } = await axios.get("/api/products");
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createProduct({
+  img_url,
+  title,
+  description,
+  price,
+  author,
+  genre,
+  inventory
+}) {
+  try {
+    const { data } = await axios.post("/api/products", {
+      img_url,
+      title,
+      description,
+      price,
+      author,
+      genre,
+      inventory
+    });
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
