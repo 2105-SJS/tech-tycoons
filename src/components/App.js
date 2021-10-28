f1919d986df90df1234aaf7c7690cb9d70268597
Brandon Fillpot <brandonfillpot@gmail.com>
2021-10-27 20:26:32 -0700

    products server running

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import {
   getSomething
 } from '../api';
 
+import Products from './Products';
+
 const App = () => {
   const [message, setMessage] = useState('');
+  const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    getSomething()
-      .then(response => {
-        setMessage(response.message);
-      })
-      .catch(error => {
-        setMessage(error.message);
-      });
-  });
+  // useEffect(() => {
+  //   getSomething()
+  //     .then(response => {
+  //       setMessage(response.message);
+  //     })
+  //     .catch(error => {
+  //       setMessage(error.message);
+  //     });
+  // });
 
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <h2>{ message }</h2>
+      <div>${message}</div>
+        <Route exact path='/products'>
+          <Products products={products} setProducts={setProducts} />
+        </Route>
     </div>
   );
 }
---
+++ src/components/Products.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { callApi } from "./util/callApi";
+
+const Products = ({ products, setProducts }) => {
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+
+            try {
+                const prodResp = await callApi({ url: '/api/products' });
+                if (prodResp) {
+                    setProducts(prodResp)
+                    return prodResp
+                }
+            } catch (error) {
+                console.error(error)
+            }
+
+        }
+        fetchProducts()
+    }, [])
+    console.log('products:', products)
+
+
+    return <>
+        <h1 className='title'>
+            Products
+        </h1>
+        <div className='content'>
+            {
+                products ? products.map(product => <>
+                    <div key={product.id} className='singleRoutine'>
+                        <h3>
+                            {product.title}
+                        </h3>
+                        <div>{product.description}</div>
+                    </div>
+                </>) : null
+            }
+        </div>
+    </>
+
+}
+export default Products
