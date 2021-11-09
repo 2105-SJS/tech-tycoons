24bc74b4cb8d6bd07745772a94ad77cebe807f92
Brandon Fillpot <brandonfillpot@gmail.com>
2021-11-08 20:12:06 -0800

    admin portal rendered conditionally

--- src/components/Register.js
+++ src/components/Register.js
@@ -49,11 +49,11 @@ const Register = ({ setToken, setUser }) => {
       }
 
     }}>
-      <input type='text' placeholder='username' value={username} onChange={(event) => setUsername(event.target.value)}></input>
-      <input type='password' placeholder='password' value={password} onChange={(event) => setPassword(event.target.value)}></input>
-      <input type='text' placeholder='First Name' value={firstName} onChange={(event) => setFirstName(event.target.value)}></input>
-      <input type='text' placeholder='Last Name' value={lastName} onChange={(event) => setLastName(event.target.value)}></input>
-      <input type='email' placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)}></input>
+      <input type='text' placeholder='username' value={username} onChange={(event) => setUsername(event.target.value)} className='input'></input>
+      <input type='password' placeholder='password' value={password} onChange={(event) => setPassword(event.target.value)} className='input'></input>
+      <input type='text' placeholder='First Name' value={firstName} onChange={(event) => setFirstName(event.target.value)} className='input'></input>
+      <input type='text' placeholder='Last Name' value={lastName} onChange={(event) => setLastName(event.target.value)} className='input'></input>
+      <input type='email' placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)} className='input'></input>
       <button type='submit'>Submit</button>
     </form>
   </>
