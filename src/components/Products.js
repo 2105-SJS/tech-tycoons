b43fdb97028dd116115844c66d2974a4bfa0de4e
“Janessa” <ortizjanessa@gmail.com>
2021-11-12 19:57:01 -0800

    styling is partially done

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { callApi } from "./util/callApi";
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 const Products = ({ products, setProducts }) => {
     const [searchTerm, setSearchTerm] = useState('')
+    const theme = createTheme();
+
     useEffect(() => {
         const fetchProducts = async () => {
 
@@ -23,35 +43,45 @@ const Products = ({ products, setProducts }) => {
 
 
     return <>
-        <h2 className='title'>
-            What Will You Read Next?
-        </h2>
-        <p>
-            <input type="text" placeholder='Search...'          className='searchBar' onChange={event => {
-                setSearchTerm(event.target.value)
-            }}
-            />
-        </p>
-        <div className='content'>
-            {
-                products ? products.filter((val) => {
-                    if(searchTerm == ''){
-                        return val
-                    } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-                        return val
-                    }
-                }).map(product => <>
-                    <div key={product.id} className='singleProduct'>
-                        <h3>{product.title}</h3>
-                        <h4>By: {product.author}</h4>
-                        <img src={product.imgURL} className='picture'/>
-                        <div><em>{product.description}</em></div>
-                        <h4>${product.price}</h4>
-                        <button>Add to cart</button>
-                    </div>
-                </>) : null
-            }
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Container sx={{ p: 4 }} maxWidth="sm">
+                <Typography variant="h3" align="center" color="text.primary" gutterBottom>What will you read next?</Typography>
+                <Grid item display='flex' alignItems='center'>
+                    <SearchIcon textIndent='left'/><TextField id="search" label="Search" type="text" placeholder='Search...' onChange={event => { setSearchTerm(event.target.value) }} autoFocus fullWidth />
+                </Grid>
+            </Container>
+            <Container sx={{ py: 3 }} maxWidth="xl">
+                <Grid container spacing={4} alignItems="flex-end">
+                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                        <div className='content'>
+                            {
+                                products ? products.filter((val) => {
+                                    if (searchTerm == '') {
+                                        return val
+                                    } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                        return val
+                                    }
+                                }).map(product => <>
+                                    <div key={product.id} className='singleProduct'>
+                                        <Typography gutterBottom p={2} variant="h6" align="center" color="black">{product.title}</Typography>
+                                        <CardContent sx={{ flexGrow: 1 }}>
+                                            <Typography>
+                                                <img src={product.imgURL} className='picture' />
+                                                <h4>By: {product.author}</h4>
+                                                <div><em>{product.description}</em></div>
+                                                <h4>${product.price}</h4>
+                                                <Button startIcon={<ShoppingCartIcon />} variant="text">Add to cart</Button>
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
+                                </>) : null
+                            }
+                        </div>
+                    </Card>
+                </Grid>
+            </Container>
+        </ThemeProvider>
     </>
 
 }
